eecdecf903d0f2135664c037ee0b7e87916bef13
tdzl2003 <tdzl2003@gmail.com>
2023-01-04 00:07:18 +0800

    -

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -9,7 +9,7 @@ import Player from '../logics/player';
 import world from '../logics/world';
 import { alert } from '../common/message';
 import { router } from '../common/history';
-import styles from './splash.less';
+import styles from './Splash.less';
 
 export default class Splash extends Component {
   async componentDidMount() {
