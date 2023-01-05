e8bcd5158f041446d53c829b68aa974bd7892493
tdzl2003 <tdzl2003@gmail.com>
2023-01-05 21:19:27 +0800

    -

--- src/logics/world.js
+++ src/logics/world.js
@@ -48,11 +48,17 @@ class World extends EventEmitter {
     this.paused = true;
     this.updateRate = 1;
     this.updatedTime = 0;
-    const totalDiffTime = Date.now() - this.player.timestamp;
-    const diffTime = Math.min(totalDiffTime, 72 * 3600 * 1000);
-    const skipTime = totalDiffTime - diffTime;
-    this.player.timestamp += skipTime;
-    this.forward(diffTime);
+    const totalDiffTime =
+      Date.now() - this.player.timestamp - this.timeline.getTime();
+    // console.log(totalDiffTime);
+    if (totalDiffTime > 0) {
+      const diffTime = Math.min(totalDiffTime, 72 * 3600 * 1000);
+      const skipTime = totalDiffTime - diffTime;
+      this.player.timestamp += skipTime;
+      this.forward(diffTime);
+    } else {
+      this.timeline.resume();
+    }
   }
 
   forward(diffTime) {
